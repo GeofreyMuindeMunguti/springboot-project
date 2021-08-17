18168f566b5c7a8417d2b054b93e2a7c4f5694c1
Muinde Geofrey <muinde@mesozi.com>
2021-08-18 01:24:58 +0300

    JWT authentication

--- src/main/java/com/example/hertz/WebSecurityConfig.java
+++ src/main/java/com/example/hertz/WebSecurityConfig.java
@@ -1,31 +1,55 @@
 package com.example.hertz;
 
+import com.example.hertz.RequestFilters.JwtTokenFilter;
 import com.example.hertz.services.MyUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.firewall.HttpFirewall;
 import org.springframework.security.web.firewall.StrictHttpFirewall;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.Arrays;
 
 @Configuration
-@EnableWebSecurity // (1)
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter { // (1)
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtTokenFilter jwtTokenFilter;
+
+    public WebSecurityConfig(JwtTokenFilter jwtTokenFilter) {
+        this.jwtTokenFilter = jwtTokenFilter;
+    }
+
+    @Override @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {  // (2)
+    protected void configure(HttpSecurity http) throws Exception {// (2)
+
+        // Dont keep cookies around (STATELESS)
+        http = http
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and();
+
+        // Authentication
         http
                 .authorizeRequests()
-                .antMatchers("/", "/users/register", "/users/home").permitAll() // URLS not requiring authentication
+                .antMatchers("/", "/auth/login", "/users/register", "/users/home").permitAll() // URLS not requiring authentication
                 .anyRequest().authenticated() // (4)
                 .and()
                 .formLogin() // (5)
@@ -36,8 +60,29 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter { // (1)
                 .permitAll()
                 .and()
                 .csrf().disable(); // (7)
+
+        // Authentication Failure handler
+        http = http
+                .exceptionHandling()
+                .authenticationEntryPoint(
+                        (request, response, ex) -> {
+                            response.sendError(
+                                    HttpServletResponse.SC_UNAUTHORIZED,
+                                    ex.getMessage()
+                            );
+                        }
+                )
+                .and();
+
+
+        // JWT filter (Processed before the request hits the API)
+        http.addFilterBefore(
+                jwtTokenFilter,
+                UsernamePasswordAuthenticationFilter.class
+        );
     }
 
+    // Headers configuration
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
@@ -51,6 +96,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter { // (1)
         return source;
     }
 
+    // Firewall setup. TODO: Make this safe.
     @Bean
     HttpFirewall httpFirewall() {
         StrictHttpFirewall firewall = new StrictHttpFirewall();
@@ -58,11 +104,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter { // (1)
         return firewall;
     }
 
+    // Specify a user detail service for use when authenticating
     @Bean
     public UserDetailsService userDetailsService() {
         return new MyUserDetailsService(); // (1)
     }
 
+    // A password encode to decode and encode passwords.
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
