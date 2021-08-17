b32bfe7f98fdbf00eafda6da7036fc783e550641
Muinde Geofrey <muinde@mesozi.com>
2021-08-17 15:57:44 +0300

    password hashing and spring security setup

--- src/main/java/com/example/hertz/controllers/UserController.java
+++ src/main/java/com/example/hertz/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.example.hertz.controllers;
 
 import com.example.hertz.models.User;
 import com.example.hertz.services.UserService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,9 +12,11 @@ import java.util.List;
 @RequestMapping(value = "users")
 public class UserController {
     private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, PasswordEncoder passwordEncoder) {
         this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @GetMapping()
@@ -21,10 +24,17 @@ public class UserController {
         return userService.getAll();
     }
 
-    @PostMapping()
+    @PostMapping(value="register")
     public User create(
             @Validated(User.Create.class)
             @RequestBody User user){
+        String hashed_password = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hashed_password);
         return userService.create(user);
+    }
+
+    @PostMapping(value="home")
+    public String Home(){
+        return "Hey home";
     }
 }
